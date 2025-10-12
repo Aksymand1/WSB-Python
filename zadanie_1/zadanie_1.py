99f92adc4ad95fa47c0fe75cdb7ec85d66107cbd
Aksymand1 <pkowalski-96@wp.pl>
2025-10-12 16:31:59 +0200

    test działania

--- zadanie_1/zadanie_1.py
+++ zadanie_1/zadanie_1.py
@@ -1,5 +1,12 @@
+# Kowalski Paweł, nr albumu 167013
+# Zadanie 1: Tworzenie i łączenie list
+
 
 import random
 
 random_list1 = [random.randint(1, 100) for _ in range(10)]
 random_list2 = [random.randint(1, 100) for _ in range(10)]
+
+zipped_list = list(zip(random_list1, random_list2))
+
+print(f"Zipped list: {zipped_list}")
