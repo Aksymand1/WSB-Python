44997ecda027a4aa8e76c6b3e43554bf6631bfb9
Aksymand1 <pkowalski-96@wp.pl>
2025-10-26 16:29:04 +0100

    Dodano zadanie z palindromem

--- zadanie_2/app.py
+++ zadanie_2/app.py
@@ -1,1 +1,19 @@
 
+#Funkcja sprawdzająca czy podany tekst jest palindromem
+def palindrom(tekst):
+    tekst_bez_spacji="".join(tekst.split())
+    dlugosc_tekstu=len(tekst_bez_spacji)
+    print(f'Sprawdzany teskt: "{tekst_bez_spacji}"')        
+    for i in range(dlugosc_tekstu//2):
+        if tekst_bez_spacji[i]!=tekst_bez_spacji[dlugosc_tekstu-1-i]:
+            return True
+        else:
+            return False
+     
+x=input("Wprowadź teskt do sprawdzenia: ")   
+if palindrom(x):
+    print(f'Podane wyrażenie: "{x}" nie jest palindromem.')
+else: 
+    print(f'Podane wyrażenie "{x}" jest palindromem.')
+
+
