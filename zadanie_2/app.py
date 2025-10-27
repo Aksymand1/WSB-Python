97d1db298cafd78e79917778e953e3e38e3ab5b2
Aksymand1 <pkowalski-96@wp.pl>
2025-10-27 16:00:14 +0100

    Poprawiona funkcja palindrom

--- zadanie_2/app.py
+++ zadanie_2/app.py
@@ -1,19 +1,19 @@
 
 #Funkcja sprawdzająca czy podany tekst jest palindromem
 def palindrom(tekst):
-    tekst_bez_spacji="".join(tekst.split())
-    dlugosc_tekstu=len(tekst_bez_spacji)
-    print(f'Sprawdzany teskt: "{tekst_bez_spacji}"')        
+    tekst_bez_spacji = "".join(tekst.split()).lower()
+    dlugosc_tekstu=len(tekst_bez_spacji)    
     for i in range(dlugosc_tekstu//2):
         if tekst_bez_spacji[i]!=tekst_bez_spacji[dlugosc_tekstu-1-i]:
-            return True
-        else:
             return False
-     
-x=input("Wprowadź teskt do sprawdzenia: ")   
-if palindrom(x):
-    print(f'Podane wyrażenie: "{x}" nie jest palindromem.')
-else: 
-    print(f'Podane wyrażenie "{x}" jest palindromem.')
+    return True
 
+palindromy = ["Kajak", "Wół utył i ma miły tułów", "A to kanapa pana Kota",
+              "Ala ma kota", "Kobyła ma mały bok", "nie jestem palindromem"]
 
+for tekst in palindromy:
+    if palindrom(tekst):
+        print(f'Podane wyrażenie: "{tekst}" jest palindromem.')
+    else: 
+        print(f'Podane wyrażenie "{tekst}" nie jest palindromem.')
+        
