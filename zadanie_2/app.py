b98f9acc09a590c385938a40558d40f31b778a8e
Aksymand1 <pkowalski-96@wp.pl>
2025-10-27 16:38:41 +0100

    Dodano klasę Sphere

--- zadanie_2/app.py
+++ zadanie_2/app.py
@@ -35,3 +35,23 @@ for email in emails:
         print(f'Podany adres email: "{email}" jest poprawny.')
     else:
         print(f'Podany adres email: "{email}" jest niepoprawny.')
+
+
+print('\n' + '='* 60 + '\n')
+
+#Funkcja obliczająca objętość kuli na podstawie promienia
+class Sphere:
+    def __init__(self, radius):
+        self.radius = radius
+           
+    def sphere_volume(radius):
+        pi = 3.14159
+        volume = (4/3) * pi * (radius ** 3)
+        return volume
+
+radii = [3, 5.5, 10, 0.25, 7.8]
+for radius in radii:
+    volume = Sphere.sphere_volume(radius)
+    print(f'Objętość kuli o promieniu {radius} wynosi: {volume:.4f}')
+
+
