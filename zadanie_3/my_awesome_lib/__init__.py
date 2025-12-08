9e94766f20bf28f4dc7979e422a860a24dae6349
Aksymand1 <pkowalski-96@wp.pl>
2025-12-08 19:52:13 +0100

    Dodano kod do pliku __init__

---
+++ zadanie_3/my_awesome_lib/__init__.py
@@ -0,0 +1,17 @@
+""" my_awesome_lib - kolekcja przydatnych narzędzi i funkcji - Laboratorium 3"""
+
+from .data_utils import usun_duplikaty, ListProcessor
+from .math_tools import suma_cyfr_liczby, srednia, silnia, fibonacci
+from .text_processing import licz_slowa, snake_to_camel
+
+__version__ = "1.0"
+__all__ = [
+    "usun_duplikaty",
+    "ListProcessor",
+    "suma_cyfr_liczby",
+    "srednia",
+    "silnia",
+    "fibonacci",
+    "licz_slowa",
+    "snake_to_camel",
+]
